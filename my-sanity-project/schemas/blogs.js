29db97a4fe772b63d8c55af2f24dba6d3facb1d7
saurabh-gaonkar <saurabhg@axioned.com>
2023-07-05 11:05:47 +0530

    added: field for layout settings

--- my-sanity-project/schemas/blogs.js
+++ my-sanity-project/schemas/blogs.js
@@ -6,6 +6,12 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'layout',
+      title: 'Layout',
+      type: 'string',
+      default: 'grid',
+    }),
+    defineField({
       name: 'title',
       title: 'Title',
       type: 'string',
