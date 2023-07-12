39181f097d9b821649bf06e66409cd8036f4ac1d
saurabh-gaonkar <saurabhg@axioned.com>
2023-07-12 18:18:56 +0530

    added internationalization packages

--- my-sanity-project/schemas/blogs.js
+++ my-sanity-project/schemas/blogs.js
@@ -1,126 +1,130 @@
 import {defineField, defineType} from 'sanity'
+import { localize } from '@sanity/document-internationalization';
 import { hotspotSchema } from './hotspot'
 
-export default defineType({
-  name: 'blog',
-  title: 'Blog',
-  type: 'document',
-  fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'object',
-      fields: [
-        {
-          name: 'en',
-          title: 'English',
-          type: 'string',
+const typeRegistry = {
+  blog: localize({
+    name: 'blog',
+    title: 'Blog',
+    type: 'document',
+    fields: [
+      defineField({
+        name: 'title',
+        title: 'Title',
+        type: 'object',
+        fields: [
+          {
+            name: 'en',
+            title: 'English',
+            type: 'string',
+          },
+          {
+            name: 'fr',
+            title: 'French',
+            type: 'string',
+          },
+        ],
+      }),
+      defineField({
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'title.en',
+          maxLength: 96,
         },
-        {
-          name: 'fr',
-          title: 'French',
-          type: 'string',
+      }),
+      defineField({
+        name: 'mainImage',
+        title: 'Main image',
+        type: 'image',
+        options: {
+          hotspot: true,
         },
-      ],
-    }),
-    defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title.en',
-        maxLength: 96,
-      },
-    }),
-    defineField({
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-      fields: [
-        {
-          name: 'hotspots',
-          title: 'Hotspots',
-          type: 'array',
-          of: [hotspotSchema],
-          options: {
-            imageHotspot: {
-              hotspotRadius: 12,
+        fields: [
+          {
+            name: 'hotspots',
+            title: 'Hotspots',
+            type: 'array',
+            of: [hotspotSchema],
+            options: {
+              imageHotspot: {
+                hotspotRadius: 12,
+              },
             },
           },
-        },
-      ],
-    }),
-    defineField({
-      name: 'categories',
-      title: 'Categories',
-      type: 'reference',
-      to: [{ type: 'category' }],
-    }),
-    defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: [{ type: 'author' }],
-    }),
-    defineField({
-      name: 'comment',
-      title: 'Comment',
-      type: 'reference',
-      to: [{ type: 'comment' }],
-    }),
-    defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    }),
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'object',
-      fields: [
-        {
-          name: 'en',
-          title: 'English',
-          type: 'blockContent',
-        },
-        {
-          name: 'fr',
-          title: 'French',
-          type: 'blockContent',
-        },
-      ],
-    }),
-    defineField({
-      name: 'bodySection2',
-      title: 'BodySection2',
-      type: 'object',
-      fields: [
-        {
-          name: 'en',
-          title: 'English',
-          type: 'blockContent',
-        },
-        {
-          name: 'fr',
-          title: 'French',
-          type: 'blockContent',
-        },
-      ],
-    }),
-  ],
+        ],
+      }),
+      defineField({
+        name: 'categories',
+        title: 'Categories',
+        type: 'reference',
+        to: [{ type: 'category' }],
+      }),
+      defineField({
+        name: 'author',
+        title: 'Author',
+        type: 'reference',
+        to: [{ type: 'author' }],
+      }),
+      defineField({
+        name: 'comment',
+        title: 'Comment',
+        type: 'reference',
+        to: [{ type: 'comment' }],
+      }),
+      defineField({
+        name: 'publishedAt',
+        title: 'Published at',
+        type: 'datetime',
+      }),
+      defineField({
+        name: 'body',
+        title: 'Body',
+        type: 'object',
+        fields: [
+          {
+            name: 'en',
+            title: 'English',
+            type: 'blockContent',
+          },
+          {
+            name: 'fr',
+            title: 'French',
+            type: 'blockContent',
+          },
+        ],
+      }),
+      defineField({
+        name: 'bodySection2',
+        title: 'BodySection2',
+        type: 'object',
+        fields: [
+          {
+            name: 'en',
+            title: 'English',
+            type: 'blockContent',
+          },
+          {
+            name: 'fr',
+            title: 'French',
+            type: 'blockContent',
+          },
+        ],
+      }),
+    ],
+    preview: {
+      select: {
+        title: 'title.en',
+        author: 'author.name',
+        media: 'mainImage',
+      },
+      prepare(selection) {
+        const { author } = selection;
+        return { ...selection, subtitle: author && `by ${author}` };
+      },
+    },
+  }),
+};
 
-  preview: {
-    select: {
-      title: 'title.en',
-      author: 'author.name',
-      media: 'mainImage',
-    },
-    prepare(selection) {
-      const { author } = selection;
-      return { ...selection, subtitle: author && `by ${author}` };
-    },
-  },
-});
+export default typeRegistry.blog
