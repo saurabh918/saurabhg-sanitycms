e6de340f7cd05382b3faa71d4f1a4a574f18bb9e
saurabh-gaonkar <saurabhg@axioned.com>
2023-07-18 12:08:29 +0530

    Updated: variables names

--- my-sanity-project/sanity.cli.js
+++ my-sanity-project/sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: SANITY_STUDIO_PROJECT_ID, 
-    dataset: SANITY_STUDIO_PROD_DATASET,
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID, 
+    dataset: process.env.SANITY_STUDIO_PROD_DATASET,
   }
 })
--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 import SanityClient from "@sanity/client";
 
 const client = new SanityClient({
-  projectId: SANITY_STUDIO_PROJECT_ID, 
-  dataset: SANITY_STUDIO_PROD_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID, 
+  dataset: process.env.SANITY_STUDIO_PROD_DATASET,
 });
 
 export default client
