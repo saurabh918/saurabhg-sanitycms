758ae6f689f8924e92a18c3e51c029158864ec8c
saurabh-gaonkar <saurabhg@axioned.com>
2023-07-18 11:55:40 +0530

    Replaced: hardcoded values with variables

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 import SanityClient from "@sanity/client";
 
 const client = new SanityClient({
-  projectId: "xkq07yg2", 
-  dataset: "production",
+  projectId: SANITY_STUDIO_PROJECT_ID, 
+  dataset: SANITY_STUDIO_PROD_DATASET,
 });
 
 export default client
