e498c5407b49af212e79a84f0ca9940725d42ab2
saurabh-gaonkar <saurabhg@axioned.com>
2023-07-20 23:04:48 +0530

    Fetched: staging environment data

--- my-sanity-project/sanity.config.js
+++ my-sanity-project/sanity.config.js
@@ -3,7 +3,7 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
+export default defineConfig([{
   name: 'default',
   title: 'My Sanity Project',
 
@@ -14,5 +14,19 @@ export default defineConfig({
 
   schema: {
     types: schemaTypes,
-  },
-})
+  }
+},
+  {
+    name: 'Staging',
+    title: 'sanity project staging',
+    basePath: '/staging',
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_STG_DATASET,
+  
+    plugins: [deskTool(), visionTool()],
+  
+    schema: {
+      types: schemaTypes,
+    },
+  }
+])
